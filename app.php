105d3b4edbeeb33b23d425193439465d4db55982
volter9 <volter925@gmail.com>
2015-12-02 08:40:27 -0800

    Include bloge/twig-renderer

--- app.php
+++ app.php
@@ -9,45 +9,7 @@
 use Bloge\Apps\AdvancedApp;
 use Bloge\Content\Advanced;
 use Bloge\Content\FrontMatter as Content;
-use Bloge\Renderers\PHP as Renderer;
-use Bloge\Renderers\IRenderer;
-
-/**
- * Comment to note:
- * 
- * I'm in process of creating repository specifically dedicated for 
- * either separate renderers adapters or whole bunch of them.
- */
-class TwigRenderer implements IRenderer
-{
-    protected $path;
-    protected $twig;
-    
-    public function __construct($path, array $options = [])
-    {
-        $this->twig = new Twig_Environment(
-            new Twig_Loader_Filesystem($path), $options
-        );
-    }
-    
-    public function twig()
-    {
-        return $this->twig;
-    }
-    
-    public function partial($view, array $data = [])
-    {
-        return $this->twig->render($view, $data);
-    }
-    
-    public function render(array $data = [])
-    {
-        $view = isset($data['view']) ? $data['view'] : '';
-        
-        return $this->partial($view, $data);
-    }
-}
-
+use Bloge\Renderers\Twig as Renderer;
 
 $content = __DIR__ . '/content';
 $theme   = __DIR__ . '/theme';
@@ -57,4 +19,4 @@ $content->processor()
     ->add(Bloge\processMerge('header', 'spyc_load'))
     ->add(Bloge\process('content', [new Parsedown, 'text']));
 
-return new AdvancedApp($content, new TwigRenderer($theme));
+return new AdvancedApp($content, new Renderer($theme));
